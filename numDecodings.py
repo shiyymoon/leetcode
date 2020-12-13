39ab3ca3599593f5f555f72e7a2052c90c38810a
shiyymoon <shiyymoon@163.com>
2020-12-13 22:27:35 +0800

    the

---
+++ numDecodings.py
@@ -0,0 +1,17 @@
+def numDecodings(self, s):
+    dp = [0 for i in range(len(s)+1)]
+    dp[0] = 1
+    if s[0] != '0':
+        dp[1] = 1
+    else:
+        return 0
+    for i in range(2,len(s)+1):
+        if s[i-1] != '0':
+            dp[i] += dp[i-1]
+        else:
+            dp[i] = 0
+        if 10<=int(s[i-2]+s[i-1])<=26:
+            dp[i] += dp[i-2]
+        else:
+            dp[i] = 0
+    
--- reverseList.py
+++ reverseList.py
@@ -4,25 +4,12 @@ def reverseList(self, head):
     :rtype: ListNode
     """
     #不懂这个解法为什么不行
-    # if (head is None || head.next is None):
-    #     return head
-    # p = head
-    # q = head
-    # while(head):
-    #     q = p.next
-    #     p.next = q.next
-    #     q.next = head
-    #     head = q
-    # return head.next
-
-    if (head is None):
+    if head is None or head.next is None: 
         return head
-    if (head.next is None): 
-        return head
-    pre = ListNode(0)
-    while(head):
-        q = head.next
-        head.next = pre
-        pre = head
+    p = head 
+    while p.next:
+        q = p.next
+        p.next = q.next
+        q.next = head
         head = q
-    return pre.next
+    return head
